a944334d14c913e496caf323cbb8ee30e4db64d8
Calvin Chia <3610680+ixiumu@users.noreply.github.com>
2025-05-05 16:50:44 +0800

    Update __init__.py
    Update methods that will be deprecated in HA 2025.6

--- huarunrq/__init__.py
+++ huarunrq/__init__.py
@@ -13,9 +13,7 @@ async def async_setup(hass: HomeAssistant, config: dict):
 async def async_setup_entry(hass: HomeAssistant, config_entry: ConfigEntry):
     """Set up HuaRunRQ from a config entry."""
     # 这里我们将传感器平台与配置条目关联起来
-    hass.async_create_task(
-        hass.config_entries.async_forward_entry_setup(config_entry, "sensor")
-    )
+    await hass.config_entries.async_forward_entry_setups(config_entry, ["sensor"])
     return True
 
 async def async_unload_entry(hass: HomeAssistant, config_entry: ConfigEntry):
